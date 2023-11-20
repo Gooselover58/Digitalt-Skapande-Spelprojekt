140291d780682bf82db92fa4de171ec04ec280a8
Gooselover58 <alexander.ejeblad@elev.ga.lbs.se>
2023-11-20 16:30:55 +0100

    Updated PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Rigidbody2D rb;
+    [SerializeField] float speed;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //GetAxisRaw innehåller ett värde som antingen är mellan -1 och 1
+        //Värdet ökar när man trycker upp, och minskar när man trycker ner
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(0, y);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
