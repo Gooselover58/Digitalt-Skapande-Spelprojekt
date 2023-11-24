2c7d1f36b9b6a7c11732a187006df53324b1a436
Gooselover58 <alexander.ejeblad@elev.ga.lbs.se>
2023-11-24 14:13:22 +0100

    Script changed

--- Assets/Scripts/PointTowardsMouse.cs
+++ Assets/Scripts/PointTowardsMouse.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PointTowardsMouse : MonoBehaviour
 {
     public Camera cam;
+    public GameObject player;
+    [SerializeField] Vector2 offset;
     private Rigidbody2D rb;
 
     void Start()
@@ -14,6 +16,7 @@ public class PointTowardsMouse : MonoBehaviour
 
     void Update()
     {
+        transform.position = (Vector2)player.transform.position + offset;
         Vector2 mouse = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector2 Dir = mouse - (Vector2)transform.position;
         float angle = Mathf.Atan2(Dir.y, Dir.x) * Mathf.Rad2Deg;
