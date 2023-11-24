8f6637e725809ba0391e1543095c99c8171d6a06
Gooselover58 <alexander.ejeblad@elev.ga.lbs.se>
2023-11-24 14:35:06 +0100

    Changed script

--- Assets/Scripts/PointTowardsMouse.cs
+++ Assets/Scripts/PointTowardsMouse.cs
@@ -11,10 +11,13 @@ public class PointTowardsMouse : MonoBehaviour
 
     void Update()
     {
+        //Sätter objektets position vid spelaren
         transform.position = (Vector2)player.transform.position + offset;
+        //Tar musens position, och använder matte för att ta reda på riktningen från objektet till musen
         Vector2 mouse = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector2 Dir = mouse - (Vector2)transform.position;
         float angle = Mathf.Clamp(Mathf.Atan2(Dir.y, Dir.x) * Mathf.Rad2Deg, -clampRot, clampRot);
+        //Ändrar riktningen av objektet
         transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
